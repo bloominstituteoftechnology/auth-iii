c5513fa4a36c24c1e3f987b836303be951984921
Luis Hernandez <luishrd@gmail.com>
2018-05-16 17:28:25 -0600

    updated packages and instructions

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
-const { secret } = require('./config');
+const secret = 'Who but me? Loves JWT';
 const port = process.env.PORT || 5000;
-const User = require('./models');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth');
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true
-};
+const User = require('./users/User');
 
 const server = express();
-server.use(bodyParser.json());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+server.use(express.json());
 server.use(cors(corsOptions));
 
 // ######################## HELPER FUNCTIONS ########################
@@ -32,82 +28,87 @@ const getTokenForUser = userObject => {
 
 const validateToken = (req, res, next) => {
   // this piece of middleware is taking the token delivered up to the server and verifying it
-  // if no token is found in the header, you'll get a 422
-  // if token is not valid, you'll be asked to login
+  // if no token is found in the header, you'll get a 422 status code
+  // if token is not valid, you'll receive a 401 status code
   const token = req.headers.authorization;
   if (!token) {
     res
       .status(422)
       .json({ error: 'No authorization token found on Authorization header' });
-    return;
+  } else {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res
+          .status(401)
+          .json({ error: 'Token invalid, please login', message: err });
+      } else {
+        // token is valid, so continue on to the next middleware/request handler function
+        next();
+      }
+    });
   }
-  jwt.verify(token, secret, (authError, decoded) => {
-    if (authError) {
-      res
-        .status(403)
-        .json({ error: 'Token invalid, please login', message: authError });
-      return;
-    }
-    // sets the decoded JWT/user object on the request object for use in next middleware.
-    req.decoded = decoded;
-    next();
-  });
 };
 
-// ######################## ROUTE HANDLERS/CONTROLLERS ########################
+// ######################## ROUTE HANDLERS ########################
 
-const createUser = (req, res) => {
+server.post('/api/users', (req, res) => {
   const { username, password } = req.body;
   const user = new User({ username, password });
+
   user.save((err, user) => {
     if (err) return res.send(err);
-    res.json({
-      success: 'User saved',
-      user
-    });
-  });
-};
 
-const getUsers = (req, res) => {
-  // This handler will not work until a user has sent up a valid JWT
-  // check out what's going on in the `validate` token function
+    const token = getTokenForUser({ username: user.username });
+    res.json({ token });
+  });
+});
+
+server.get('/api/users', validateToken, (req, res) => {
   User.find({}, (err, users) => {
     if (err) return res.send(err);
+
     res.send(users);
   });
-};
+});
 
-const login = (req, res) => {
+server.post('/api/login', (req, res) => {
   const { username, password } = req.body;
   User.findOne({ username }, (err, user) => {
     if (err) {
-      res.status(500).json({ error: 'Invalid Username/Password' });
-      return;
+      return res.status(500).json({ error: 'Invalid Username/Password' });
     }
-    if (user === null) {
-      res.status(422).json({ error: 'No user with that username in our DB' });
-      return;
+
+    if (!user) {
+      return res
+        .status(422)
+        .json({ error: 'No user with that username in our DB' });
     }
-    user.checkPassword(password, (nonMatch, hashMatch) => {
-      // This is an example of using our User.method from our model.
-      if (nonMatch !== null) {
-        res.status(422).json({ error: 'passwords dont match' });
-        return;
+
+    user.checkPassword(password, (err, isMatch) => {
+      if (err) {
+        return res.status(422).json({ error: 'passwords dont match' });
       }
-      if (hashMatch) {
+
+      if (isMatch) {
         const token = getTokenForUser({ username: user.username });
         res.json({ token });
+      } else {
+        return res.status(422).json({ error: 'passwords dont match' });
       }
     });
   });
-};
-
-// ######################## ROUTES ########################
-
-server.get('/api/users', validateToken, getUsers);
-server.post('/api/users', createUser);
-server.post('/api/login', login);
-
-server.listen(port, (req, res) => {
-  console.log(`server listening on port ${port}`);
 });
+
+// ######################## CONNECT TO DB AND START THE API ########################
+
+mongoose
+  .connect('mongodb://localhost/auth')
+  .then(() => {
+    console.log('\n=== Connected to MongoDB ===');
+    server.listen(port, (req, res) => {
+      console.log(`\n=== API up on port ${port} ===\n`);
+    });
+  })
+  .catch(err =>
+    console.log('\n=== Error connecting to MongoDb, is it running? ===\n', err)
+  );
