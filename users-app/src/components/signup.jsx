1d1f04307bd9d6a631ac8f18dbe6eed7840ceacc
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-17 16:31:20 -0400

    signup component without redirect

---
+++ users-app/src/components/signup.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SignUp extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleSubmit = () => {
+    const { username, password } = this.state;
+    axios
+      .post("http://localhost:5000/api/users", { username, password })
+      .then(user => localStorage.setItem('token', user.data.token))
+      .catch(err => console.log(err));
+    this.setState({ username: "", password: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <input 
+          type="text"
+          name="username"
+          value={this.state.username}
+          placeholder="username"
+          onChange={event => this.setState({ [event.target.name]: event.target.value })}
+        />
+        <input 
+          type="password"
+          name="password"
+          value={this.state.password}
+          placeholder="password"
+          onChange={event => this.setState({ [event.target.name]: event.target.value })}
+        />
+        <button onClick={() => this.handleSubmit()}>Register</button>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
