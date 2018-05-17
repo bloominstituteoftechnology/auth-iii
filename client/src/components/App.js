db3c66762b5b6ab7f11f403a5ab53b50937a4368
Jared Cuffe <jcuffe@gmail.com>
2018-05-17 16:41:27 -0700

    Client-side OAuth flow handled at App level

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import Register from './Register'
 import UsersList from './UsersList'
 import Login from './Login'
+import OAuth from './OAuth'
+import { clientID, oAuthState} from '../secrets'
 
 class App extends Component {
 
@@ -12,23 +14,30 @@ class App extends Component {
     this.props.history.push('/')
   }
 
+  githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientID}&state=${oAuthState}`
+
   controls = () => {
     if (localStorage.getItem('token')) {
       return <button onClick={this.logout}>Logout</button>
     } else {
-      return [
-        <Link to='/register'><button>Create User</button></Link>,
-        <Link to='/login'><button>Log in</button></Link>
-      ]
+      return (
+        <div>
+          <Link to='/register'><button>Create User</button></Link>
+          <Link to='/login'><button>Log in</button></Link>
+          <a href={this.githubOAuthUrl}><button>Login with github</button></a>
+        </div>  
+      )
     }
   }
 
   render = () => (
     <div>
       {this.controls()}
+      {localStorage.getItem('token')}
       <Route path='/register' exact component={Register} />
       <Route path='/login' component={Login} />
       <Route path='/users' component={UsersList} />
+      <Route path='/callback' component={OAuth} />
     </div>
   )
 }
