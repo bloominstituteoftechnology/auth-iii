ed052da54242edff1877523abec6557d8bf5e1a3
Jared Cuffe <jcuffe@gmail.com>
2018-05-17 10:26:17 -0700

    Smoothed login/logout flow with redirects and controls

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router'
+import React, { Component } from 'react'
+import { Route, withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
 import Register from './Register'
 import UsersList from './UsersList'
 import Login from './Login'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route path='/' exact component={Register} />
-        <Route path='/login' component={Login} />
-        <Route path='/users' component={UsersList} />
-      </div>
-    );
+
+  logout = () => {
+    localStorage.removeItem('token')
+    this.props.history.push('/')
   }
+
+  controls = () => {
+    if (localStorage.getItem('token')) {
+      return <button onClick={this.logout}>Logout</button>
+    } else {
+      return [
+        <Link to='/register'><button>Create User</button></Link>,
+        <Link to='/login'><button>Log in</button></Link>
+      ]
+    }
+  }
+
+  render = () => (
+    <div>
+      {this.controls()}
+      <Route path='/register' exact component={Register} />
+      <Route path='/login' component={Login} />
+      <Route path='/users' component={UsersList} />
+    </div>
+  )
 }
 
-export default App;
+export default withRouter(App)
