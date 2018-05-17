f3453b418a71fe30832370cbdfd032fce65ec89b
Jared Cuffe <jcuffe@gmail.com>
2018-05-17 10:13:48 -0700

    Added browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter> 
+  , document.getElementById('root'))
