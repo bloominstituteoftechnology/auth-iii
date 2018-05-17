7d9818053f57c7f6f45e5eeec7ed87843fbe8123
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-17 12:17:25 -0700

    filled out app.js with routes and imports and button/button function

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, withRouter } from 'react-router-dom';
+
+import Signup from './auth/Signup';
+import Signin from './auth/Signin';
+import Users from './users/Users';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {localStorage.getItem('token') && (
+            <button onClick={this.signout}>Sign Out</button>
+          )}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
+        <Route path='/users' component={Users} />
       </div>
     );
   }
+
+  signout = () => {
+    localStorage.removeItem('token');
+    this.props.history.push('/signin');
+  }
 }
 
-export default App;
+export default withRouter(App);
