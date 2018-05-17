f9825221c6a842e0afb9ee5e5c6a124d6a853445
cgoez <goez2001@gmail.com>
2018-05-17 14:25:29 -0700

    submitRegisterHandler event

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,70 +1,69 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Login extends React.Component {
   state = {
-    username: '',
-    password: '',
-  }
+    username: "",
+    password: ""
+  };
 
   submitLoginHandler = event => {
     event.preventDefault();
     axios
-      .post('http://localhost:5000/api/login', this.state)
+      .post("http://localhost:5000/api/login", this.state)
       .then(res => {
-        localStorage.setItem('token', res.data.token);
-        this.props.history.push('/api/users');
+        localStorage.setItem("token", res.data.token);
+        this.props.history.push("/api/users");
       })
       .catch(err => {
-        localStorage.removeItem('token');
+        localStorage.removeItem("token");
       });
-    }
+  };
 
   submitRegisterHandler = event => {
     event.preventDefault();
     axios
-        .post('http://localhost:5000/api/register', this.state)
-        .then(res => {
-          localStorage.setItem('token', res.data.token);
-          this.props.history.push('/api/users');
+      .post("http://localhost:5000/api/register", this.state)
+      .then(res => {
+        response: "Welcome!"
       })
-        .catch(err => {
-        localStorage.removeItem('token');
+      .catch(err => {        
+          error: "There  was an error during registration."        
       });
-    }
+  };
 
   inputHandler = event => {
     const { name, value } = event.target;
 
     this.setState({ [name]: value });
-  }
+  };
 
   render() {
     return (
       <form>
         <div>
-          <input 
-            name='username'
-            type='text'
+          <input
+            name="username"
+            type="text"
             value={this.state.username}
             onChange={this.inputHandler}
-            placeholder='Username'
+            placeholder="Username"
           />
         </div>
         <div>
-          <input 
-            name='password'
-            type='password'
+          <input
+            name="password"
+            type="password"
             value={this.state.password}
             onChange={this.inputHandler}
-            placeholder='Password'
+            placeholder="Password"
           />
         </div>
         <div>
-            <button onClick={this.submitLoginHandler}>Login</button>
+          <button onClick={this.submitLoginHandler}>Login</button>
         </div>
         <div>
-            <button onClick={this.submitRegisterHandler}>Register</button>
+          <button onClick={this.submitRegisterHandler}>Register</button>
         </div>
       </form>
     );
@@ -72,10 +71,10 @@ class Login extends React.Component {
 }
 
 class Register extends React.Component {
-    state = {
-        username: 'lambda',
-        password: 'class',
-    };
+  state = {
+    username: "lambda",
+    password: "class"
+  };
 }
 
 export default Login;
