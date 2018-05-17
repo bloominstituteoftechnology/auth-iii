fef0ca5f9b801437e06afb99faf123d943558b8b
Jared Cuffe <jcuffe@gmail.com>
2018-05-17 16:40:04 -0700

    Secrets for Github OAuth

---
+++ client/src/secrets.js
@@ -0,0 +1,9 @@
+const oAuthState = 'Lambda'
+const clientSecret = 'ef79e14604410c2d2a5947250939fb02b5c31c99'
+const clientID = 'fe3d344222baf2b3cf8e'
+
+module.exports = {
+  oAuthState,
+  clientSecret,
+  clientID
+}
