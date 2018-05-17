e1d79fb42b24bbea79b80f2b317f14760d579057
Devon Bacon <devon.bacon@gmail.com>
2018-05-17 13:40:20 -0700

    App.js - imported Login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
-
+import Login from './Components/Login/Login.js';
 
 class App extends Component {
   render() {
@@ -16,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login />
       </div>
     );
   }
