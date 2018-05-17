154602de9a013da710a651f7059e305a3281e33c
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-17 16:31:14 -0400

    Made component files for signup, signin, users

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import Signin from './auth/Signin';
+import Users from './users/Users';
 
 class App extends Component {
   render() {
@@ -10,7 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-          
+
       </div>
     );
   }
