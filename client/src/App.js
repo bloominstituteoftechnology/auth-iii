f97b4e6d4066e1cb1cbe67e6c3bafb19f5a352c1
Chris <cjmarsh725@gmail.com>
2018-05-17 14:18:53 -0700

    Created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
@@ -9,13 +13,17 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+
+          {localStorage.getItem('token') && (
+            <button onClick={this.signout}>Sign Out</button>
+          )}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/users" component={Users} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/components/Users.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+
+class Users extends React.Component {
+  state = {
+    users: [],
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.users.map(user => <li key={user._id}>{user.username}</li>)}
+      </ul>
+    );
+  }
+
+  componentDidMount = event => {
+    const token = localStorage.getItem('token');
+
+    const authToken = `Bearer ${token}`;
+
+    const requestOptions = {
+      headers: {
+        Authorization: authToken,
+      },
+    };
+
+    axios
+      .get('http://localhost:5000/api/users', requestOptions)
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(err => {
+        this.props.history.push('/signin');
+      });
+  };
+}
+
+export default Users;
