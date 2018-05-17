78764a25a55efe39b6c6313785163ea1833e0ba6
Velsu <riftmage50@gmail.com>
2018-05-17 23:18:42 +0200

    Added basic front-end UI and fixed token auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route, withRouter, NavLink } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Users from "./components/Users";
 import Register from "./components/Register";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
@@ -13,13 +14,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            <NavLink className="home" to="/">
+              Home
+            </NavLink>
+          </h1>
 
           {localStorage.getItem("token") && (
-            <button onClick={this.signout}>Sign out</button>
+            <button className="logout" onClick={this.signout}>
+              Sign out
+            </button>
           )}
         </header>
 
+        <Route exact path="/" component={Home} />
         <Route path="/signin" component={Login} />
         <Route path="/signup" component={Register} />
         <Route path="/users" component={Users} />
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 class Users extends React.Component {
   state = {
-    users: []
+    users: [] || ["fetching users..."]
   };
 
   render() {
