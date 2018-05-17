62aa40355cb500c156287318e04b12b3a567feb3
Devon Bacon <devon.bacon@gmail.com>
2018-05-17 13:33:58 -0700

    Lofin.js - Set up login component and state

---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+}
+
+export default Login;
