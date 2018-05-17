9d487551485a44f8ed97f62404c302b2806ee897
Jared Cuffe <jcuffe@gmail.com>
2018-05-17 10:13:59 -0700

    Basic top-level routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router'
+import Register from './Register'
+import UsersList from './UsersList'
+import Login from './Login'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Route path='/' exact component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/users' component={UsersList} />
       </div>
     );
   }
