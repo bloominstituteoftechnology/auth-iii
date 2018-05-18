f68a02ecdd385873014de82dbb4ba3a44d49f3f4
Nedu Robert <nedumrobert@gmail.com>
2018-05-18 01:10:43 +0100

    added Home component

---
+++ client/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="HomeContainer">
+      <h1>UsersBook</h1>
+    </div>
+  );
+};
+
+export default Home;
